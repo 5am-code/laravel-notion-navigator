8f8fcb38a545e8356e0f6a35b407a38a187144d9
johguentner <johannes.guentner@it-guentner.de>
2021-05-28 15:32:34 +0200

    remove comments

--- resources/views/notion_database.blade.php
+++ resources/views/notion_database.blade.php
@@ -49,38 +49,14 @@ use Carbon\Carbon;
     <div id="page_modal" class="fixed inset-0 z-10 hidden overflow-y-auto" aria-labelledby="modal-title" role="dialog"
         aria-modal="true">
         <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-            <!--
-                                    Background overlay, show/hide based on modal state.
-
-                                    Entering: "ease-out duration-300"
-                                      From: "opacity-0"
-                                      To: "opacity-100"
-                                    Leaving: "ease-in duration-200"
-                                      From: "opacity-100"
-                                      To: "opacity-0"
-                                  -->
             <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" aria-hidden="true"></div>
-
-            <!-- This element is to trick the browser into centering the modal contents. -->
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-            <!--
-                                    Modal panel, show/hide based on modal state.
-
-                                    Entering: "ease-out duration-300"
-                                      From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                      To: "opacity-100 translate-y-0 sm:scale-100"
-                                    Leaving: "ease-in duration-200"
-                                      From: "opacity-100 translate-y-0 sm:scale-100"
-                                      To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                  -->
             <div class="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-6xl sm:w-full sm:p-6"
                 style="height:800px;">
                 <div class="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
                     <button type="button" onclick="closePage();"
                         class="text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <span class="sr-only">Close</span>
-                        <!-- Heroicon name: outline/x -->
                         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
